9f8dc55ed942fe0b4474e67c988990fbd7d28bbe
aryanagarwal15 <f2016705@goa.bits-pilani.ac.in>
2017-08-05 18:21:31 +0530

    Reg page fixes

--- static/js/snow.js
+++ static/js/snow.js
@@ -5,7 +5,7 @@ window.onload = function(){
 	
 	//canvas dimensions
 	var W = window.innerWidth;
-	var H = window.innerHeight/2;
+	var H = window.innerHeight;
 	canvas.width = W;
 	canvas.height = H;
 	
